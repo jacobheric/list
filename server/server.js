0a6ef54b53356d18df3ca029787a60cff9b7c017
Jacob Heric <jacob@jacobheric.com>
2013-03-26 14:26:39 -0400

    Don't pre-populate and empty list

--- server/server.js
+++ server/server.js
@@ -1,16 +1,3 @@
 Meteor.startup(function() {
 	
-	if (Things.find().count() > 0) {
-		return;
-	}
-	
-    var names = ["Milk",
-                 "Trash Bags",
-                 "Wine",
-                 "Beer",
-                 "Depends"];
-
-    for (var i = 0; i < names.length; i++){
-      Things.insert({name: names[i].toLowerCase()});
-	}	
 });
